80264a2c534c64ffe36979abe3e60e4f06b4b68a
hotsothearith <rith47782@gmail.com>
2026-01-24 02:28:34 +0700

    fix error with url api

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -3,5 +3,5 @@ class AppConfig {
   // - Android emulator: http://10.0.2.2:8000/api
   // - iOS simulator:    http://127.0.0.1:8000/api
   // - Real device:      http://<your-laptop-ip>:8000/api
-  static const String baseUrl = 'http://10.0.2.2:8000/api';
+  static const String baseUrl = 'http://127.0.0.1:8000/api';
 }
